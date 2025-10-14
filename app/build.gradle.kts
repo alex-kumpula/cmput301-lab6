b1847ff102e9685f8de2fb3a763597de6eeb4cd8
Alex Kumpula <alex.kumpula01@gmail.com>
2025-10-14 17:24:50 -0600

    Generate docs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+//    implementation(files("/home/alex/Android/Sdk/platforms/android-36/android.jar"))
 }
