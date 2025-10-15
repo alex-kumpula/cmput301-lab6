b4436521f42ce64b6dfd38aa67b0e2e88c41c3e4
Alex Kumpula <alex.kumpula01@gmail.com>
2025-10-14 18:06:20 -0600

    Create tests

--- app/src/main/java/com/example/listycity/CityList.java
+++ app/src/main/java/com/example/listycity/CityList.java
@@ -32,4 +32,38 @@ public class CityList {
         Collections.sort(list);
         return list;
     }
+
+    /**
+     * When given a city, returns whether or not it belongs in the list.
+     * @param city
+     * The city to check.
+     * @return
+     * True if the city is in the list, else, false.
+     */
+    public boolean hasCity(City city) {
+        return this.cities.contains(city);
+    }
+
+    /**
+     * Check if a city is present in the list. If it does then remove
+     * it from the list, if not then throws an exception.
+     * @param city
+     * The city to delete from the list.
+     */
+    public void delete(City city) {
+        // Throw an exception if the given city does not exist in the list.
+        if (!this.hasCity(city)) {
+            throw new IllegalArgumentException();
+        }
+        this.cities.remove(city);
+    }
+
+    /**
+     * Returns how many cities are in the list.
+     * @return
+     * How many cities are in the list.
+     */
+    public int countCities() {
+        return this.cities.size();
+    }
 }
--- app/src/test/java/com/example/listycity/CityListTest.java
+++ app/src/test/java/com/example/listycity/CityListTest.java
@@ -49,4 +49,86 @@ class CityListTest {
         assertEquals(0, city.compareTo(cityList.getCities().get(0)));
         assertEquals(0, mockCity().compareTo(cityList.getCities().get(1)));
     }
+
+    @Test
+    void testHasCity() {
+        // Create a new city list
+        CityList cityList = new CityList();
+
+        // Add two cities to the list
+        City city0 = new City("Charlottetown", "Prince Edward Island");
+        City city1 = this.mockCity();
+        cityList.add(city0);
+        cityList.add(city1);
+
+        // Assert that the hasCity method returns true for both
+        assertTrue(cityList.hasCity(city0));
+        assertTrue(cityList.hasCity(city1));
+
+        // Create another city, but do not add it to the list
+        City city2 = this.mockCity();
+
+        // Assert that the list hasCity method does not return
+        // true for city2 which is not in the list
+        assertFalse(cityList.hasCity(city2));
+    }
+
+    @Test
+    void testDeleteCity() {
+        // Create a new city list
+        CityList cityList = new CityList();
+
+        // Add two cities to the list
+        City city0 = new City("Charlottetown", "Prince Edward Island");
+        City city1 = this.mockCity();
+        cityList.add(city0);
+        cityList.add(city1);
+
+        // Assert that the city list has two cities, which are
+        // also the ones we added
+        assertEquals(2, cityList.getCities().size());
+        assertTrue(cityList.getCities().contains(city0));
+        assertTrue(cityList.getCities().contains(city1));
+
+        // Delete city0
+        cityList.delete(city0);
+
+        // Assert that the city list only has city1 now
+        // and is also of length 1
+        assertEquals(1, cityList.getCities().size());
+        assertFalse(cityList.getCities().contains(city0));
+        assertTrue(cityList.getCities().contains(city1));
+    }
+
+    @Test
+    void testDeleteException() {
+        // Create a new city list with one city
+        CityList cityList = mockCityList();
+
+        // Create a new city
+        City city = new City("Yellowknife", "Northwest Territories");
+
+        // Make sure cityList.delete throws an exception when
+        // we try to delete a city it does not contain
+        assertThrows(IllegalArgumentException.class, () -> {
+            cityList.delete(city);
+        });
+    }
+
+    @Test
+    void testCountCities() {
+        // Create a new city list with zero cities
+        CityList cityList = new CityList();
+
+        // Assert that the city count is 0
+        assertEquals(0, cityList.countCities());
+
+        int numCitiesToAdd = 42;
+        for (int i = 0; i < numCitiesToAdd; i++) {
+            cityList.add(this.mockCity());
+        }
+
+        // Assert that the city count equals numCitiesToAdd
+        assertEquals(numCitiesToAdd, cityList.countCities());
+    }
 }
